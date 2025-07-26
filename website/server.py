d2bd844999fde599c3a34f3355e4f43c0fe567bc
HT LAPTOP <HT LAPTOP@DESKTOP-J9GATOO>
2025-07-26 14:13:33 +0500

    prototype done

--- website/server.py
+++ website/server.py
@@ -5,6 +5,7 @@ load_dotenv()
 from flask import Flask, request, jsonify, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import smtplib
 # from flask_admin import Admin
 # from flask_admin.contrib.sqla import ModelView
 
@@ -78,7 +79,16 @@ def contact():
 
         db.session.add(new_contact)
         db.session.commit()
+        sender = 'webfamtrix@gmail.com'
+        reciever = 'famtrixsolutions@gmail.com'
+        subject = f'Client email: {full_name}'
+        text = f"Subject: {subject}\n\n{message}\n\nemail: {email} \n\nphone: {phone}"
 
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.starttls()
+
+        server.login(sender, os.getenv('APP_PASSWORD'))
+        server.sendmail(sender, reciever, text)
         return redirect(url_for('contact'))  # Or show success message
     
     return render_template('contact.html')
@@ -92,3 +102,5 @@ def services():
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
+
+
