6a0a3f7c5c942c6f9c3128c50307808e67614d39
MUHAMMAD FAZEEL <153188860+FazeelAr@users.noreply.github.com>
2025-07-26 14:44:48 +0500

    Revert "Frontend"

--- website/admin.py
+++
@@ -1,8 +0,0 @@
-from flask_admin import Admin
-from flask_admin.contrib.sqla import ModelView
-
-# Initialize admin
-admin = Admin(app, name='Admin Panel', template_mode='bootstrap4')
-
-# Add your models
-admin.add_view(ModelView(ContactRequest, db.session))
